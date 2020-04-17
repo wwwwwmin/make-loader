683dd7c651cd7f176654e5696140dc74844f7eb9
Nacy Mulligan <43332601+wwwwwmin@users.noreply.github.com>
2020-04-17 17:19:55 +0800

    异步loader使用this.async

--- loaders/replaceLoader.js
+++ loaders/replaceLoader.js
@@ -1,24 +1,14 @@
 const loaderUtils = require('loader-utils');
 
-// 不能写成箭头函数，因为要使用this,
 module.exports = function(source){
-    //this.query为options参数，官方推荐使用loader-utils（https://webpack.js.org/api/loaders/#thisquery）
-    console.log(this.query);
-    // return source.replace('dell',this.query.name)
-/* 
-    将文件里的dell均改成dellLee 
-    return source.replace('dell','dellLee')
-*/
     const options = loaderUtils.getOptions(this);
-    const result= source.replace('dell',options.name)
+    // 声明异步loader
+    // this.async
+    // Tells the loader-runner that the loader intends to call back asynchronously. Returns this.callback
+    const callback = this.async();
 
-    this.callback(null,result)
-    
+    setTimeout(() => {
+        const result= source.replace('dell',options.name);
+        callback(null,result);
+    },1000);
 }
-// 代替return，返回更多的东西
-// this.callback(
-//     err: Error | null,
-//     content: string | Buffer,
-//     sourceMap?: SourceMap,
-//     meta?: any
-//   );
